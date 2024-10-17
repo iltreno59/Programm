61965df3a662b65f7bd7f31f0afeb70311988919
iltreno59 <nekitkuprik05@gmail.com>
2024-10-17 19:16:01 +0300

    practice 2 final commit

--- parser_yahoo.js
+++ parser_yahoo.js
@@ -14,7 +14,7 @@ const csvWriter = createCsvWriter({
         { id: 'away_team', title: 'Away team name' },
         { id: 'home_score', title: 'Home goals' },
         { id: 'away_score', title: 'Away goals' },
-        { id: 'week', title: 'Week'},
+        { id: 'week', title: 'Week' },
         { id: 'competition', title: 'Competition' }
     ]
 });
@@ -23,52 +23,52 @@ const matches = [];
 let date_range = 1;
 const date_range_max = 7;
 
-function send_request(date_range){
-    return new Promise((resolve) =>{
-const options = {
-    url: `https://sports.yahoo.com/soccer/scoreboard/?confId=&dateRange=${date_range}&schedState=2`,
-    method: 'GET'
-};
+function send_request(date_range) {
+    return new Promise((resolve) => {
+        const options = {
+            url: `https://sports.yahoo.com/soccer/scoreboard/?confId=&dateRange=${date_range}&schedState=2`,
+            method: 'GET'
+        };
 
-request(options, (err, res, body) => {
-    if (err) {
-        return console.log(err);
-    }
-    const $ = cheerio.load(body);
-    for(let i = 0; i < 20; i += 2){
-    try{
-    const element_team = $("div.score span[class='YahooSans Fw(700)! Fz(14px)!']"); // HOME - 0, AWAY - 1
-    //console.log(element_team[1].children[0].data);
-    const element_score = $("div.score span[class='YahooSans Fw(700)! Va(m) Fz(24px)!']"); // HOME - 0, AWAY - 1
-    //console.log(element_score[1].children[0].data);
-    const element_week = $("option[selected]");
-    //console.log(element_week[1].children[0].data.split(' ')[1].trim());
-    const number = iteration;
-    const match = {
-        number: number,
-        home_team: element_team[i].children[0].data,
-        away_team: element_team[i + 1].children[0].data,
-        home_score: element_score[i].children[0].data,
-        away_score: element_score[i + 1].children[0].data,
-        week: element_week[1].children[0].data.split(' ')[1].trim(),
-        competition: competition
-    }
-    matches.push(match);
-    iteration++;
-    }
-    catch(err){
-        continue;
-    }
-    }
-    resolve(); 
-});
+        request(options, (err, res, body) => {
+            if (err) {
+                return console.log(err);
+            }
+            const $ = cheerio.load(body);
+            for (let i = 0; i < 20; i += 2) {
+                try {
+                    const element_team = $("div.score span[class='YahooSans Fw(700)! Fz(14px)!']"); // HOME - 0, AWAY - 1
+                    //console.log(element_team[1].children[0].data);
+                    const element_score = $("div.score span[class='YahooSans Fw(700)! Va(m) Fz(24px)!']"); // HOME - 0, AWAY - 1
+                    //console.log(element_score[1].children[0].data);
+                    const element_week = $("option[selected]");
+                    //console.log(element_week[1].children[0].data.split(' ')[1].trim());
+                    const number = iteration;
+                    const match = {
+                        number: number,
+                        home_team: element_team[i].children[0].data,
+                        away_team: element_team[i + 1].children[0].data,
+                        home_score: element_score[i].children[0].data,
+                        away_score: element_score[i + 1].children[0].data,
+                        week: element_week[1].children[0].data.split(' ')[1].trim(),
+                        competition: competition
+                    }
+                    matches.push(match);
+                    iteration++;
+                }
+                catch (err) {
+                    continue;
+                }
+            }
+            resolve();
+        });
     });
 }
 async function main() {
-    while (date_range <= date_range_max){
+    while (date_range <= date_range_max) {
         await send_request(date_range);
         date_range++;
-        await new Promise(resolve => setTimeout(resolve, 100)); 
+        await new Promise(resolve => setTimeout(resolve, 100));
     }
 
     console.log(matches.length);
